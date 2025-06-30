44c0a88c3570ad26d8b75cfebe55b892bc1c7206
Luna <lunatattersall@gmail.com>
2025-06-30 23:54:33 +0100

    Header Update, Short Bio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
+import Logo from "../assets/Portfolio-LOGO.jpg";
+
 export default function Header({ path }: { path: string }) {
   return (
     <header className="bg-[#2c2c2c] h-[73px] text-white flex justify-between items-center px-8 md:px-16 lg:px-5.5 rounded-lg">
       <div className="Logo-Name text-[25px] font-bold flex gap-2 items-center">
-        <p className="mb-2">Luna's Links :3</p>
+        <img src={Logo} width={55} />
+        <p className="mb-1">Luna's Links</p>
       </div>
 
       <div className="flex justify-end items-start" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,13 @@ import Connections from "../components/home/Connections";
 export default function Home() {
   return (
     <>
+      <p className="text-3xl flex pt-3 font-medium">
+        Hai, Welcome to my website!
+      </p>
+      <p className="flex pt-2 font-light">
+        This is a very basic website just for the links enjoy i will fix while
+        learning
+      </p>
       <Connections />
     </>
   );
