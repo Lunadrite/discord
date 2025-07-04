656c86f33a0bbb72f81b2eba4c2d7f06f97d7087
Luna <lunatattersall@gmail.com>
2025-07-04 16:53:07 +0100

    Connections Center Fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,9 @@ import Connections from "../components/home/Connections";
 export default function Home() {
   return (
     <main className="h-fit">
-      <div className=" pt-3 font-medium text-center mx-auto">
+      <div className=" pt-3 font-medium text-center">
         <p className="text-3xl ">Hai, Welcome to my website!</p>
-        <p className="font-light  mx-autotext-center">
+        <p className="font-light ">
           This is a very basic website just for the links enjoy i will fix while
           learning
         </p>
