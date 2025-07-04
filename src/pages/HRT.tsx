54a02336f6a17a5c375f16dbca6cdf0edc18db5c
Luna <lunatattersall@gmail.com>
2025-07-04 04:54:15 +0100

    HRT products

--- src/pages/HRT.tsx
+++ src/pages/HRT.tsx
@@ -1,7 +1,108 @@
 export default function HRT() {
   return (
     <>
-      <p>meow</p>
+      <blockquote className="flex items-center justify-center border-1 border-pink-200 mt-5 w-[900px] break-text mx-auto rounded-3xl">
+        <p className="text-2xl font-bold text-cente">
+          Many people int he UK ask how i got my HRT, especially with the new
+          laws which are used to try hide trans people so i hope this will help
+          someone who needs it!
+        </p>
+      </blockquote>
+      <div className="flex items-center">
+        <h1 className="flex items-center pt-5 text-start text-2xl font-bold mx-auto">
+          Buying your prodcuts
+        </h1>
+      </div>
+      <div className="flex items-center">
+        <h1 className="flex items-center text-start mx-auto pt-3 font-bold text-pink-200 text-xl underline underline-offset-5">
+          Estrogen!
+        </h1>
+      </div>
+      <div className="flex flex-col items-center border-1 mt-3 w-[500px] mx-auto rounded-2xl">
+        <h1 className="flex items-center text-center mx-auto font-bold text-blue-100 text-lg">
+          Astrovials
+          <a
+            target="_blank"
+            href="https://astrovials.com/"
+            className=" font-thin text-xs pl-2"
+          >
+            link
+          </a>
+        </h1>
+        <p className="p-2">
+          I have had a great expirence with them, Their payment system was easy
+          and the shipping only took 3 weeks to get to me!
+        </p>
+      </div>
+      <div className="flex flex-col items-center border-1 mt-4 w-[650px] mx-auto rounded-2xl">
+        <h1 className="flex items-center text-center mx-auto font-bold text-blue-100 text-lg">
+          Otokonoko Pharmaceuticals (OTK)
+          <a
+            target="_blank"
+            href="https://otkph.am/"
+            className=" font-thin text-xs pl-2 "
+          >
+            link
+          </a>
+        </h1>
+        <p className="p-2">
+          I have had a great expirence with them also! Really reliable but they
+          are sadly slow it took me 2 months for it to arrive, but its my
+          favourite vial yet plus adorable packaging! :(
+        </p>
+      </div>
+      <div className="flex items-center">
+        <h1 className="flex items-center text-start mx-auto pt-3 font-bold text-blue-200 text-xl underline underline-offset-5">
+          Needles and Wipes!
+        </h1>
+      </div>
+      <div className="flex flex-col items-center border-1 mt-3 w-[500px] mx-auto rounded-2xl">
+        <h1 className="flex items-center text-center mx-auto font-bold text-pink-200 text-lg">
+          Medisave
+          <a
+            target="_blank"
+            href="https://www.medisave.co.uk/"
+            className=" font-thin text-xs pl-2"
+          >
+            link
+          </a>
+        </h1>
+        <p className="p-2">
+          This is the only website i would suggest if your in the uk, Not only
+          is it safe and medical grade but its also cheap to help on costs!
+          <p className="font-bold text-red-400">
+            Please be safe if your doing DIY and dont reuse needles
+          </p>
+        </p>
+      </div>
+      <div className="flex items-center">
+        <h1 className="flex items-center text-start mx-auto pt-3 font-bold text-pink-200 text-xl underline underline-offset-5">
+          Blood Tests!
+        </h1>
+      </div>
+      <div className="flex flex-col items-center border-1 mt-3 w-[500px] mx-auto rounded-2xl">
+        <h1 className="flex items-center text-center mx-auto font-bold text-blue-200 text-lg">
+          Randox
+          <a
+            target="_blank"
+            href="https://www.randox.com/"
+            className=" font-thin text-xs pl-2"
+          >
+            link
+          </a>
+        </h1>
+        <p className="p-2">
+          I highly suggest using randox! Its where i have been going since
+          starting HRT and the nurses are amazing and really are sweethearts,
+          The only problem is that its only 2 days every few months, But its
+          good to do a blood test and treat yourself after!
+        </p>
+      </div>
+      <div className="flex items-center">
+        <h1 className="flex items-center pt-5 text-start text-2xl font-bold mx-auto">
+          Injecting!
+        </h1>
+      </div>
     </>
   );
 }
