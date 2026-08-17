6b28f9e824d204a1b87e3fdca1804bf18e307a73
Luna <lunatattersall@gmail.com>
2026-08-17 20:06:46 +0100

    Boom finished BaseSystem Blogpost

--- src/pages/BaseSystemsBlogpost.tsx
+++ src/pages/BaseSystemsBlogpost.tsx
@@ -4,6 +4,9 @@ import BinarMeme from "../assets/Blogposts/BaseSystems/BinaryMEME.jpg"
 import binarbreakdown from "../assets/Blogposts/BaseSystems/BinaryBreakdown.png"
 import binaryshowcase from  "../assets/Blogposts/BaseSystems/Binary-Showcase.png"
 import william from "../assets/Blogposts/BaseSystems/William_Sellers_small.png"
+import hexmeme from "../assets/Blogposts/BaseSystems/hexmeme.png"
+import basesystemrep from "../assets/Blogposts/BaseSystems/Base-10-16-Showcase.png"
+import hexbreakdown from "../assets/Blogposts/BaseSystems/Hex-Breakdown.png"
 
 export default function Blogposts() {
     return(
@@ -32,7 +35,23 @@ export default function Blogposts() {
             <Blog.Text>Well, we have a problem there, don't we? That’s what John William Nystrom thought too and made the system called Hexadecimal, which we cover in this next part :)</Blog.Text>
             <Blog.Image src={william}/>
             <Blog.SubHeading>Hexadecimal</Blog.SubHeading>
-
+            <Blog.Image src={hexmeme}/>
+            <Blog.SubHeading>Why do we use Hexadecimal?</Blog.SubHeading>
+            <Blog.Text>We use hexadecimal because it’s useful to display large numbers using fewer digits, </Blog.Text>
+            <Blog.Text>For example, representing color values and MAC addresses. In addition to this, once you learn Hex, it’s easier to read than binary! </Blog.Text>
+            <Blog.Text>Being able to show large numbers is used to display memory to the user, and we use to to define the location in memory we want to use, or display. Doing this in Binary would be way too large to read and keep track of.  </Blog.Text>
+            <Blog.Text>So let’s get started on how to read it!</Blog.Text>
+            <Blog.Image width="50%" src={basesystemrep}/>
+            <Blog.Text>In this diagram, you can see that in base 16 we continue from 9 and go to A, </Blog.Text>
+            <Blog.Text>A in Hex is 10</Blog.Text>
+            <Blog.Text>F in Hex is 15,</Blog.Text>
+            <Blog.Text>Knowing this, you can fill in the gaps! So let’s go on to learn how to read examples such as 0xF8</Blog.Text>
+            <Blog.Image width="50%" src={hexbreakdown}/>
+            <Blog.Callout>“0x” is just a notation device we use to show we are using Hexadecimal.</Blog.Callout>
+            <Blog.SubHeading>Conclusion</Blog.SubHeading>
+            <Blog.Text>You should have the idea of what the basic Base addresses are now! </Blog.Text>
+            <Blog.Text>Remember that learning a new base system can be hard, so don't worry about not getting it straight away! Even if you come back to it and know it once, you need to learn it.</Blog.Text>
+            <Blog.Text>Thank you for reading this article, and I hope it helped you learn more about Base Addresses if you where confused! </Blog.Text>
         
         
         </div>
